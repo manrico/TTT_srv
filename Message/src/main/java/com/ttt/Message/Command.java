9d832825581e06a61d0f274bd001dc3a14e19380
Urmet Saar <urmet@mooncascade.com>
2015-10-10 15:01:08 +0300

    submodule removal pt. 2

---
+++ Message/src/main/java/com/ttt/Message/Command.java
@@ -0,0 +1,8 @@
+package com.ttt.Message;
+
+/**
+ * Created by urmet on 9.10.15.
+ */
+public enum Command {
+    HELLO, BYE, PING, ERROR
+}
---
+++ Message/src/main/java/com/ttt/Message/Message.java
@@ -0,0 +1,15 @@
+package com.ttt.Message;
+
+import java.io.Serializable;
+
+/**
+ * Created by urmet on 9.10.15.
+ */
+public class Message implements Serializable {
+    public Command cmd;
+    public String payload;
+    public Message(Command c, String pl) {
+        cmd = c;
+        payload = pl;
+    }
+}
