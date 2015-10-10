c9d869a70ca88c6669aa63e33a0bdaeea3114756
Urmet Saar <urmet@mooncascade.com>
2015-10-10 14:54:15 +0300

    shuffle things around

---
+++ Client/src/main/java/Client.java
@@ -0,0 +1,18 @@
+/**
+ * Created by urmet on 9.10.15.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Client extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
