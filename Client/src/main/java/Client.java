2cbe1881c1dbee8af2b56f33d9cb1556c0b66589
Martti Muttik <marttimuttik@gmail.com>
2015-10-21 22:33:41 +0300

    Added server communication

--- Client/src/main/java/Client.java
+++ Client/src/main/java/Client.java
@@ -2,6 +2,9 @@
  * Created by urmet on 9.10.15.
  */
 
+import com.ttt.Message.ClientCommand;
+import com.ttt.Message.Message;
+import com.ttt.Message.ServerCommand;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
@@ -15,16 +18,24 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class Client extends Application {
 
     private boolean turnX = true;
+    private Socket             socket;
+    private ObjectOutputStream oos;
+    private ObjectInputStream  ois;
 
     private Parent createContent() {
 
         Pane board = new Pane();
-        board.setPrefSize(800, 600);            // Akna loomine
+        board.setPrefSize(800, 600);            // Creates Window Pane
 
-        // Mänguruudustik
+        // Game board
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 Tile tile = new Tile();
@@ -38,9 +49,28 @@ public class Client extends Application {
 
 
     public void start(Stage primaryStage) throws Exception {
+        this.socket = new Socket("localhost", 5843);
+        try {
+            this.oos = new ObjectOutputStream(this.socket.getOutputStream());
+            this.ois = new ObjectInputStream(this.socket.getInputStream());
+
+        } catch (IOException e) {
+            System.out.println("Error" + e.getMessage());
+        }
+
+        Message message = new Message(ClientCommand.REGISTER, "Marko");
 
         primaryStage.setScene(new Scene(createContent()));
         primaryStage.show();
+
+        Message serverMessage;
+
+        /*while ((serverMessage = (Message) ois.readObject()) != null) {
+
+            System.out.println("The message from client:  " + serverMessage.toString());
+            Message output = handleMessage(serverMessage);
+            sendMessage(output);
+        }*/
     }
 
     private class Tile extends StackPane {
@@ -51,15 +81,17 @@ public class Client extends Application {
             border.setFill(null);
             border.setStroke(Color.BLACK);
 
-            text.setFont(Font.font(50));        // X-de ja 0-de suurus
-            setAlignment(Pos.CENTER);           // x-de 0-de paiknevus "kastis"
+            text.setFont(Font.font(50));
+            setAlignment(Pos.CENTER);
             getChildren().addAll(border, text);
 
             setOnMouseClicked(event -> {
                 if (event.getButton() == MouseButton.PRIMARY) {
                     drawX();
-                } else if (event.getButton() == MouseButton.SECONDARY){
+                    Message message = new Message(ClientCommand.REGISTER, "Marko");
+                } else if (event.getButton() == MouseButton.SECONDARY) {
                     draw0();
+                    Message message = new Message(ClientCommand.REGISTER, "Marko");
                 }
             });
         }
@@ -77,28 +109,35 @@ public class Client extends Application {
         launch(args);
 
     }
-}
 
+    public Message handleMessage(Message message) {
+        Message answer;
+        switch ((ServerCommand) message.cmd) {
+            // Test connection
+            case REGISTER_OK:
+                System.out.println("Got register OK");
+                answer = new Message(ServerCommand.PONG, null);
+                break;
 
-
-    /*@Override
-    public void start(Stage primaryStage) {
-        Message message = new Message(ClientCommand.REGISTER, "Marko");
-
-
-
+            default:
+                answer = new Message(ServerCommand.ERROR, "No comprendo?");
+        }
+        return answer;
     }
+
     public void sendMessage(Message message) {
+
+        System.out.println("Sending to server : " + message.toString());
         try {
-            Socket socket = new Socket("localhost", 5843);
-            System.out.println("Sending to server : " + message.toString());
-            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
             oos.writeObject(message);
             oos.flush();
-        } catch (Exception e) {
-            // don't catch root exception!
-            System.out.println("unknown host!");
+
+        } catch (IOException e) {
+            System.out.println("Error" + e.getMessage());
+
         }
     }
 }
-*/
+
+
+
