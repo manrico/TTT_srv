f9260af4ab1311b670431fb9e2ef296c0f9a0178
Martti Muttik <marttimuttik@gmail.com>
2015-11-16 17:49:17 +0200

    Added Client_3 class

---
+++ Client/src/main/java/Client_3.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by kasutaja on 16.11.2015.
+ */
+public class Client_3 extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
