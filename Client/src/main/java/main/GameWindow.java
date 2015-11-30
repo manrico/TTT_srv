bd9b7d5e475d7fb3db119845ad0c3938aba7f33b
Martti Muttik <marttimuttik@gmail.com>
2015-11-30 21:26:27 +0200

    Removed class Game and added all code to GameWindow class. Added disableMouse, enableTileMouseEvents & sendMessage methods to GameWindow class.

--- Client/src/main/java/main/GameWindow.java
+++ Client/src/main/java/main/GameWindow.java
@@ -1,10 +1,21 @@
 package main;
 
+import com.ttt.Message.ClientCommand;
+import com.ttt.Message.Message;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Iterator;
 
 /**
  * Created by X1 on 24.11.2015.
@@ -14,6 +25,10 @@ public class GameWindow {
     private Pane gameScene;
     private Scene scene2;
     private String boardBackground;
+    private ObjectOutputStream oos;
+    private Rectangle border;
+    private Image xPic = new Image(getClass().getResourceAsStream("X.png"));
+    private Image oPic = new Image(getClass().getResourceAsStream("O.png"));
 
     GameWindow() {
         createContent();
@@ -21,7 +36,7 @@ public class GameWindow {
 
     private Pane createContent() {
         gameScene = new Pane();
-        gameScene.setPrefSize(450, 450);
+        gameScene.setPrefSize(800, 600);
         boardBackground = Game.class.getResource("Grid.png").toExternalForm();
         gameScene.setStyle("-fx-background-image: url('" + boardBackground + "'); " +
                 "-fx-background-position: top left; " +
@@ -45,4 +60,78 @@ public class GameWindow {
         }
         return gameScene;
     }
+
+    // TODO move this out from here.
+    public static void handleMessage(Message message) {
+    }
+
+    private class Game extends StackPane {
+        private Label fill = new Label();
+
+        public Game() {
+            // Tic Tac Toe board with rectangles, can be uncommented as were using background image as well.
+            border = new Rectangle(150, 150);
+            border.setFill(null);
+            border.setStroke(Color.BLACK);
+            border.setOpacity(0);
+            getChildren().addAll(border, fill);
+
+            this.enableMouse();
+        }
+
+        private void enableMouse() {
+            setOnMouseClicked(event -> {
+                drawMark(2);
+                Message message = new Message(ClientCommand.DECISION, this.getId());
+                sendMessage(message);
+            });
+        }
+
+        private void sendMessage(Message message) {
+            this.disableMouse();
+            this.enableTileMouseEvents();
+
+            System.out.println("Sending to server : " + message.toString());
+            try {
+                oos.writeObject(message);
+                oos.flush();
+
+            } catch (IOException e) {
+                System.out.println("Error" + e.getMessage());
+            }
+        }
+
+        private void enableTileMouseEvents() {
+            for (Iterator<javafx.scene.Node> i = gameScene.getChildren().iterator(); i.hasNext(); ) {
+                Game currentPane = (Game) i.next();
+                if (currentPane.fill.equals("")) {
+                    currentPane.enableMouse();
+                }
+            }
+        }
+
+        private void disableMouse() {
+            for (Iterator<Node> i = gameScene.getChildren().iterator(); i.hasNext(); ) {
+                i.next().setOnMouseClicked(null);
+            }
+        }
+
+        private void drawMark(int mark) {
+            switch (mark) {
+                case 0:
+                    fill.setText("");
+                    break;
+                case 1:
+                    fill.setGraphic(new ImageView(xPic));
+                    break;
+                case 2:
+                    fill.setGraphic(new ImageView(oPic));
+                    break;
+                default:
+                    fill.setText(""); //TODO build exception / handling
+                    break;
+            }
+        }
+    }
 }
+
--- Client/src/main/java/main/Main.java
+++ Client/src/main/java/main/Main.java
@@ -1,15 +1,38 @@
 package main;
 
+import com.ttt.Message.Message;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 /**
  * Created by X1 on 24.11.2015.
  */
 public class Main extends Application {
+    private ObjectInputStream ois;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         setUserAgentStylesheet(STYLESHEET_CASPIAN);
         new FirstWindow();
+
+        // separate non-FX thread for server listening
+        new Thread() {
+            Message serverMessage;
+
+            // runnable for that thread
+            public void run() {
+                try {
+                    while ((serverMessage = (Message) ois.readObject()) != null) {
+                        System.out.println("The message from server:  " + serverMessage.toString());
+                        GameWindow.handleMessage(serverMessage);
+                    }
+                } catch (IOException | ClassNotFoundException e) {
+                    System.out.println("Error on handling Server communication : " + e.getMessage());
+                }
+            }
+        }.start();
     }
 }
